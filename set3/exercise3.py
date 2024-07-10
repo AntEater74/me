2df6ef6ad48974e4384f36d2a3a4504130fad1a0
Zack Tate <z5479356@ad.unsw.edu.au>
2024-07-10 16:15:10 +1000

    exersise 3 edit

--- set3/exercise3.py
+++ set3/exercise3.py
@@ -30,7 +30,7 @@ def advancedGuessingGame():
     """
     import random
     
-    
+    988
     while True:
         try:
             lower_bound = int(input("Enter the lower bound for the guessing range: "))
